094085d7f5037a13599c276d93f62fcef61c0c4b
Johnettdigitxp <johnett.mathew@digitxp.com>
2018-11-19 14:12:54 +0530

    Unwanted comments removed from MainActivity.

--- app/src/main/java/com/gittrio/www/MainActivity.kt
+++ app/src/main/java/com/gittrio/www/MainActivity.kt
@@ -2,13 +2,21 @@ package com.gittrio.www
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.microsoft.appcenter.AppCenter
+import com.microsoft.appcenter.analytics.Analytics
+import com.microsoft.appcenter.crashes.Crashes;
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-//Testing comments goes here.
-        //This is my first branch in github
+        //Only useful codes used in this activity.
+
+
+        AppCenter.start(getApplication(),
+            "29b84e98-3617-4f34-a6b8-32dce04845bc",
+            Analytics::class.java,
+                    Crashes::class.java)
     }
 }
